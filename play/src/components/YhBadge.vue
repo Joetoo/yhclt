8124b3ceadf0d3698d9491a8bdb1375b641df471
Joe <1570466620@qq.com>
2024-08-04 00:25:21 +0800

    ci: 🎡 hbs模板

---
+++ play/src/components/YhBadge.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <h4>YhBadge组件</h4>
+  <div>
+    <YhBadge type="primary" text="primary" />
+    <YhBadge type="success" text="success" />
+    <YhBadge type="info" text="info" />
+    <YhBadge type="error" text="error" />
+    <YhBadge type="warning" text="warning" />
+  </div>
+</template>
+
+<style scoped lang="sass">
+
+</style>
