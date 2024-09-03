1318d3caa870481eebe9ea8788bff053f9a6a958
Joe <1570466620@qq.com>
2024-09-03 14:40:53 +0800

    chore: 移除ui包build脚本中Sass构建

---
+++ packages/ui/scripts/build.ts
@@ -0,0 +1,16 @@
+// #!/usr/bin/env node
+
+// import { exec } from 'node:child_process'
+// import color from 'picocolors'
+// import { build } from 'vite'
+// import config from '../vite.config'
+
+// build(config).then(() => {
+//   exec('JoeFe-cli build-sass', (err, stdout, stderr) => {
+//     if (!err) {
+//       console.log('success', color.cyan(stdout.toString()))
+//     } else {
+//       console.log('err', color.red(stderr.toString()))
+//     }
+//   })
+// })
